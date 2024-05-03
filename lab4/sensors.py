16909edb8be4c310b2df50e43683e7635ecfc60a
ZaAuchan <145759532+ZaAuchan@users.noreply.github.com>
2024-05-03 15:04:46 +0700

    Add files via upload

--- lab4/sensors.py
+++ lab4/sensors.py
@@ -9,42 +9,36 @@ import queue
 logging.basicConfig(filename='app.log', level=logging.INFO)
 flag = True
 
-
 class Sensor:
     def get(self):
         raise NotImplementedError("Subclasses must implement method get()")
 
-
 class SensorX(Sensor):
     """Sensor X"""
-
     def __init__(self, delay: float):
         self._delay = delay
         self._data = 0
 
-    def get(self) -> int:
+    def get(self, frame=None) -> int:
         time.sleep(self._delay)
         self._data += 1
         return self._data
-
 
 class SensorCam(Sensor):
     def __init__(self, cam, res):
         if cam == 'default':
             self.cap = cv2.VideoCapture(0)
         else:
-            self.cap = cv2.VideoCapture(cam)
+            self.cap = cv2.VideoCapture(int(cam))
         self.cap.set(3, res[0])
         self.cap.set(4, res[1])
 
     def get(self):
         ret, frame = self.cap.read()
-
         return frame, ret
 
     def release(self):
         self.cap.release()
-
 
 class WindowImage:
     def __init__(self, freq):
@@ -65,14 +59,12 @@ class WindowImage:
     def close(self):
         cv2.destroyWindow("window")
 
-
-def process(que, sensor):
+def process_sensor(que, sensor):
     global flag
     while flag:
-        new_sens = sensor.get()
-        if que.empty():
-            que.put(new_sens)
-
+        frame = que.get()
+        new_sens = sensor.get(frame)
+        que.put(frame)  # Put the frame back after processing
 
 def main(args):
     global flag
@@ -85,39 +77,39 @@ def main(args):
 
     if not camera.cap.isOpened():
         logging.info('The camera is turned off.')
+        print('The camera is turned off.')
         camera.release()
         window.close()
         sys.exit()
 
-    queue1 = queue.Queue()
-    queue2 = queue.Queue()
-    queue3 = queue.Queue()
+    frame_queue = queue.Queue(maxsize=1)
+    frame_queue.put(None)  # Initializing the queue with None
 
-    thread1 = threading.Thread(target=process, args=(queue1, sensor1))
-    thread2 = threading.Thread(target=process, args=(queue2, sensor2))
-    thread3 = threading.Thread(target=process, args=(queue3, sensor3))
+    thread1 = threading.Thread(target=process_sensor, args=(frame_queue, sensor1))
+    thread2 = threading.Thread(target=process_sensor, args=(frame_queue, sensor2))
+    thread3 = threading.Thread(target=process_sensor, args=(frame_queue, sensor3))
 
     thread1.start()
     thread2.start()
     thread3.start()
 
-    sensor1 = sensor2 = sensor3 = 0
     while True:
-        if not queue1.empty():
-            sensor1 = queue1.get()
-        if not queue2.empty():
-            sensor2 = queue2.get()
-        if not queue3.empty():
-            sensor3 = queue3.get()
-        sensim, ret = camera.get()
+        frame, ret = camera.get()
         if not ret or not camera.cap.isOpened() or not camera.cap.grab():
             logging.info('The camera had turned off.')
+            print('The camera had turned off.')
             camera.release()
             window.close()
             flag = False
             sys.exit()
 
-        window.show(sensim, sensor1, sensor2, sensor3)
+        frame_queue.put(frame)  # Put the frame in the queue for sensors to process
+
+        sensor1_data = sensor1.get(frame)
+        sensor2_data = sensor2.get(frame)
+        sensor3_data = sensor3.get(frame)
+
+        window.show(frame, sensor1_data, sensor2_data, sensor3_data)
         time.sleep(1 / window.freq)
 
         if cv2.waitKey(1) == ord('q'):
@@ -125,7 +117,6 @@ def main(args):
             window.close()
             flag = False
             sys.exit()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
